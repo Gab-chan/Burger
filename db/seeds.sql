cdc5324d4fcec6bc0fb63c6a68f827b76c38d8a9
Gabriel <pedromurillo96@gmail.com>
2019-08-27 13:41:05 -0700

    major updates to all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('CheeseBurger');
+INSERT INTO burgers (burger_name) VALUES ('BaconBurger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('DoubleBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('TripleBurger', true);
+
